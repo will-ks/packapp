f78c6a27c384b1427835240624c6deb4c870b395
dambusm <willschaller@gmail.com>
2018-10-15 12:11:19 +0200

    Add router, forms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CreatePageComponent } from './pages/create-page/create-page.component';
+
+// --- Routes --- //
+const routes: Routes = [
+  {
+    path: '',
+    component: CreatePageComponent
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CreatePageComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
