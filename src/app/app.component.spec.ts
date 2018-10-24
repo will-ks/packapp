d8a469f0646f0138fedd5c8a408fffd91478c206
dambusm <willschaller@gmail.com>
2018-10-24 18:38:22 +0200

    Rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'packapp2-client'`, () => {
+  it(`should have as title 'packapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('packapp2-client');
+    expect(app.title).toEqual('packapp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to packapp2-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to packapp!');
   });
 });
